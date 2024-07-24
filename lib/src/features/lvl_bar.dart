c4f0b03c4a69fa7405a55dc3bafc76905111ee2a
Ben <benny.14@hotmail.de>
2024-07-24 11:32:43 +0200

    Skill Tree

---
+++ lib/src/features/lvl_bar.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class ExperienceBar extends StatefulWidget {
+  ExperienceBar({super.key, this.currentXp, this.lvl, this.maxXp}) {
+    throw UnimplementedError();
+  }
+  int? currentXp;
+  int? maxXp;
+  int? lvl;
+
+  void lvlUp() {
+    if (currentXp! >= maxXp!) {
+      lvl! + 1;
+      print('Congrats! you have reached Lvl $lvl');
+      currentXp = currentXp! - maxXp!;
+    }
+  }
+
+  @override
+  State<ExperienceBar> createState() => _ExperienceBarState();
+}
+
+class _ExperienceBarState extends State<ExperienceBar> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
