119ba18f10319c9d505ec3d7924e048307071912
Hoovey@gmx.de <hoovey@gmx.de>
2024-07-25 08:57:04 +0200

    BLA änderung1

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,8 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('H79c4h7\n1889\nwir schaffen das Kollege'),
+          child: Text(
+              'H79c4h7\n1889\nwir schaffen das Kollege\n\nMoiiiiiiiiinMoin'),
         ),
       ),
     );
